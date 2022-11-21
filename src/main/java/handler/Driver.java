42c778bb1519e20d44770e5750b639603107f049
sudhanshu chaudhary <sudhanshu chaudhary@LAPTOP-J27E1TSD>
2022-11-21 21:41:03 +0530

    21/11/2022 Assignment.

--- src/main/java/handler/Driver.java
+++ src/main/java/handler/Driver.java
@@ -1,8 +1,101 @@
 package handler;
 
-public class Driver {
-	public static void main(String[]args) {
-		System.out.println("This is my first git comit.");
-	}
+import java.util.Scanner;
+public class Driver{
+	
+	public static void main(String[] args){
+		
+		
 
+	// Question number 1.
+//		Scanner sc= new Scanner (System.in);
+//		System.out.println("Enter the Number");
+//		int number =sc.nextInt();
+//		if (number>0) {
+//			System.out.println("The Number is positive.");
+//		}
+//		else {
+//			System.out.println("The number is negative.");
+//		}
+//	}
+//}
+		
+		
+		
+		
+		
+		
+		
+		// Question number 2.
+	Scanner f= new Scanner (System.in);
+	System.out.println("Enter the number");
+	int number =f.nextInt();
+	if (number==1) {
+	System.out.println("Monday ");
+	    }
+	else if (number==2) {
+		System.out.println("Tuesday");
+	}
+	else if(number==3) {
+		System.out.println("Wednesday ");
+	}
+	else if (number==4) {
+		System.out.println("Thursday");
 }
+	else if (number==5) {
+		System.out.println("Friday");
+			}
+	else if (number==6) {
+
+		System.out.println("Saturday");
+}
+	else if (number==7) {
+		System.out.println("Sunday");
+	}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		// Question number 3.
+//	Scanner x=new Scanner(System.in);
+//	System.out.println("Enter the number");
+//	              int number=x.nextInt();
+//	 Scanner y=new Scanner(System.in);
+//	 System.out.println("Enter the number");
+//	 int b=y.nextInt();
+//	 
+//	 Scanner z=new Scanner(System.in);
+//	 System.out.println("Enter the number");
+//	 int f=z.nextInt();
+//	 if(x>y) {
+//		 System.out.println("")
+//	 }
+	 
+		
+		
+		
+		
+		//Question number 4.
+	 
+//	 Scanner l=new Scanner(System.in);
+//	 System.out.println("Type Alphabet=");
+//	 char c=((l.nextLine()).charAt(0));
+//	
+//	
+//	;
+//	if(character == a) {
+//		 System.out.println("This Alphabet is vowel.");
+//	 }
+//	 else {
+//	 System.out.println("This Alphabet is consonent.");
+//	 
+//	 
+// }
+}
+}
+	
